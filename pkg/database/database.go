e34fc116fb3c93bdb86d537265da5b4e023cc613
Tomasz Wójcik <elroyski@gmail.com>
2025-03-20 21:54:57 +0100

    Aktualizacja projektu

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -25,7 +25,7 @@ func InitDB() {
 	log.Printf("Database: %s", dbname)
 	log.Printf("Port: %s", port)
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=no-verify",
 		host, user, password, dbname, port)
 
 	log.Printf("DSN: %s", dsn)
